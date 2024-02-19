b496543e4e9ece3b23018dba6176e781e87592f4
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-02-19 14:34:36 +0000

    Add post and make migrations

--- my_project/settings.py
+++ my_project/settings.py
@@ -24,13 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ms)0ied5$)^kqudc)gy%#a(b-*6n-0+dnpzmvo(!z@y(2wm^nv'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-efthymiakak-projectmana-39rpggzgns8.ws-eu108.gitpod.io'
-,'.herokuapp.com']
+,'project-management-tool-70101b30ec1e.herokuapp.com']
 
 
 # Application definition
@@ -89,6 +89,11 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://8000-efthymiakak-projectmana-39rpggzgns8.ws-eu108.gitpod.io/",
+    "https://project-management-tool-70101b30ec1e.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
