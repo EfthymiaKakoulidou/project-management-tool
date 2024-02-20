e682fcc65686c00d2bd3ef5f3d66ef2031a59cae
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-02-20 22:37:19 +0000

    Add authentication

--- my_project/settings.py
+++ my_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-efthymiakak-projectmana-39rpggzgns8.ws-eu108.gitpod.io'
 ,'project-management-tool-70101b30ec1e.herokuapp.com']
