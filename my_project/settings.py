211ced4420214692cf74cf2f6229a0cdc6c03256
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-03-05 11:24:56 +0000

    Style images for profile detail and profile list

--- my_project/settings.py
+++ my_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 import dj_database_url
 if os.path.exists('env.py'):
     import env
@@ -117,6 +118,8 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
