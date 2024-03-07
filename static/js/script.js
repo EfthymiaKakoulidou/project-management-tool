27da738042524eecc388bc8ef7f1edade444f30e
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-03-07 09:01:48 +0000

    Style dropdown to push the content of the page when visible

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,15 @@
 function toggleMenu() {
-    var navList = document.getElementById('navbarNavDropdown');
-    navList.classList.toggle('show');
+  var navbar = document.querySelector('.navbar');
+  var navbarHeight = navbar.offsetHeight;
+  var navbarCollapse = document.querySelector('.navbar-collapse');
+
+  if (navbarCollapse.classList.contains('show')) {
+    // Hide the menu
+    navbarCollapse.classList.remove('show');
+    navbar.style.marginBottom = '0';
+  } else {
+    // Show the menu
+    navbarCollapse.classList.add('show');
+    navbar.style.marginBottom = navbarHeight;
   }
+}
