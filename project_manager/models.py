9e5b508301be25f875a9a9fc25ebba1c80a8eebf
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-03-09 13:23:07 +0000

    Update profile model to accept the same job title

--- project_manager/models.py
+++ project_manager/models.py
@@ -45,7 +45,7 @@ class Profile(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name="name"
     )
-    job_title = models.CharField(max_length=200, unique=True, null=False, blank=False)
+    job_title = models.CharField(max_length=200, null=False, blank=False)
     bio = models.TextField(null=False, blank=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
