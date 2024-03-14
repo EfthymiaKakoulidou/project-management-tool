a7ba07d2f633b2adec6e0b5ae4a1a4f08713524d
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-03-14 14:41:20 +0000

    Add error pages to views

--- project_manager/views.py
+++ project_manager/views.py
@@ -294,3 +294,12 @@ class Home(TemplateView):
         random_motivation = random.choice(motivational_quotes)
         context['random_motivation'] = random_motivation
         return context
+
+# Error views
+    
+def page_not_found_view(request, exception):
+    return render(request, '404.html', status=404)
+
+
+def server_error_view(request):
+    return render(request, '500.html', status=500)
