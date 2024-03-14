b98f92d7c2a9c26eb84943a5a8d42bc9f8b96ae6
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-03-14 11:21:22 +0000

    Add SSL security for heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -166,3 +166,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
